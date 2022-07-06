6d449e1400da22b45c1f273d1b74355838c7898d
Victoria Coker <vic-e@Victorias-MacBook-Air.local>
2022-07-05 20:21:42 -0400

    added a class

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ class Game {
 }
 
 class Tamagotchi {
-  constructor()
+  constructor(name, age, food, sleep, fun)
+  this.name = name
+  this.age = 0
+  this.food = 1
+  this.sleep = 1
+  this.boredom = 1  
 }
